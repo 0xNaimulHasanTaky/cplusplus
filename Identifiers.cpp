bcf4e3efc8f82c55061c124f48ba82765abe3684
Naimul Hasan Taky <naimulhasantaky@proton.me>
2025-07-18 22:54:02 +0600

    check again

--- Identifiers.cpp
+++ Identifiers.cpp
@@ -6,5 +6,6 @@
 // C++ is a case-sensitive programming language. Thus, Manpower and manpower are two different identifiers in C++.
 // Here are some examples of acceptable identifiers −
 
+
 // mohd       zara    abc   move_name  a_123
 // myname50   _temp   j     a23b9      retVal
