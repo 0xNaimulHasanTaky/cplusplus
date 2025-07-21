339dfbf4075a9acf7316aeebe426747ba82523bd
Naimul Hasan Taky <naimulhasantaky@proton.me>
2025-07-21 20:33:45 +0600

    update Hello_World.cpp

--- Hello_World.cpp
+++ Hello_World.cpp
@@ -1,26 +1,26 @@
-// Header file inclusion section:
+// Preprocessor Directive or Header file inclusion section
+// The #include is known as a pre-processor directive in C++.
+// It is used to include header files with specific methods and elements.
+// Multiple #include statements are used to apply different header files in the program.
 // This is the section where we include all required header files whose functions we are going to use in the program.
+// The iostream is the header file that defines functions and operations related to the input/output stream.
 #include <iostream>
 
-// Namespace section: This is the section where we use the namespace.
+// Namespaces are used to differentiate code blocks with the same method names. In this program,
+// the using namespace std; is used to set the namespace as standard for users to apply all standard methods in programs.
 // The line using namespace std; tells the compiler to use the std namespace.
 using namespace std;
 
-// The main() section:
-// In this section, we write our main code.
-// The line int main() is the main function where program execution begins.
+// The main() Function (int main(){...})
+// The main() function is the default starting point of any C++ program. It is compulsory for any C++ program to have a main function.
+// The program logics are written inside the main program. The main function body is enclosed inside parenthesis ({}).
 int main() {
-
+    // Printing Statement (cout), The print/output statement is cout followed by "<<" operator. 
     cout << "Hello World"; // prints Hello World
 
     // return 0; terminates main() function and causes it to return the value 0 to the calling process.
-    // In C++11 and later standards,
-    // if control reaches the end of the main() function without a return statement,
-    // the compiler automatically adds return 0; as a default.
-    // This is equivalent to successful execution of the program.
-    // From the C++ standard (C++11 and later):
-    // [basic.start.main]: If control reaches the end of main() without encountering a return statement,
-    // the effect is as if return 0; were executed.
-
+    // In C++11 and later standards, [basic.start.main]:
+    // If control reaches the end of the main() function without a return statement,
+    // The compiler automatically adds return 0; as a default. The effect is as if return 0; were executed.
     return 0; // Optional
 }
