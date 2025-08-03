89ac1584a4e405e72b89e89e1f78a0080cc22b02
Naimul Hasan Taky <naimulhasantaky@proton.me>
2025-08-03 20:31:14 +0600

    add Boolean_Literals.cpp

---
+++ Boolean_Literals.cpp
@@ -0,0 +1,4 @@
+// There are two Boolean literals{Literals.cpp} and they are part of standard C++ keywords{Keywords.cpp}.
+// A value of true representing true.
+// A value of false representing false.
+// We should not consider the value of true equal to 1 and value of false equal to 0.
